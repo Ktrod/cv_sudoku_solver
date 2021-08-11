f62927f2bf490c791c0fedc1958ed9f7afda8bd6
Keenan Troddyn <keenan.troddyn@gmail.com>
2021-08-11 13:09:50 -0400

    Added functionality to printTable(). Added bigger table in cv_sudoku_solver.cpp for testing.

--- src/solver.cpp
+++ src/solver.cpp
@@ -20,6 +20,31 @@ bool Sudoku::isValid() const {
 
 void Sudoku::printTable() const {
 
-    std::cout << "Print table" << std::endl;
+    for (int i = 0; i < table[0].size() + ((table[0].size() / 3) + 1); i++) std::cout << ". "; //Print top row 
+
+    std::cout << "" << std::endl;
+    for (int i = 0; i < table.size(); i++){
+        if (i % 3 == 0 && i != 0){ //Check if we've reach third horizontal box and print seperator line
+            for (int k = 0; k < table[0].size() + ((table[0].size() / 3) + 1); k++) std::cout << ". "; 
+            std::cout << "" << std::endl;
+            std::cout << ".";
+        }
+        else {
+            std::cout << ".";
+        }
+        for (int j = 0; j < table.size(); j++){
+            std::cout << " ";
+            std::cout << table[i][j];
+
+            if (j + 1 == table.size()) { //Check if we've reach end of line and print seperator char plus a new line
+                std::cout << " ." << std::endl;
+            }
+            else if ((j+1) % 3 == 0 && j != 0) { //Check if we've reached third vertical box and print seperator char 
+                std::cout << " .";
+            }
+        }
+    }
+
+    for (int i = 0; i < table[0].size() + ((table[0].size() / 3) + 1); i++) std::cout << ". "; //Print bottom row
 
 }
